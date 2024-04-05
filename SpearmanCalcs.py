eb121e101362dd15232146aec31c7315a1d1d5f1
Susan Janet Hamilton <83848566+SusanJHamilton@users.noreply.github.com>
2024-04-05 17:11:44 -0500

    Spearman correlation calcs

---
+++ SpearmanCalcs.py
@@ -0,0 +1,17 @@
+from scipy.stats import spearmanr
+import pandas as pd
+
+df = pd.read_csv('data_with_gas_usage.csv', on_bad_lines='skip')
+
+ 
+# calculate Spearman's correlation coefficient and p-value
+corr, pval = spearmanr(df['GASUSGE'],df['URBAN'], nan_policy='omit')
+ 
+for column in df:
+    print(column)
+    corr, pval = spearmanr(df['GASUSGE'],df[column], nan_policy='omit')
+    print("Spearman's correlation coefficient:", corr)
+    print("p-value:", pval)
+# print the result
+print("Spearman's correlation coefficient:", corr)
+print("p-value:", pval)
